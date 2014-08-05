367f2272fb9e8eb452957c584d461d8a5a8abc7e
gilad1987 <gilad19872@gmail.com>
2014-08-05 16:51:23 +0300

    Youtube directive

--- src/js/youtube/controllers/Youtube.ctrl.js
+++ src/js/youtube/controllers/Youtube.ctrl.js
@@ -2,6 +2,8 @@
 
     function YouTubeController($scope, YT_events){
 
+        this.YT_events = YT_events;
+
         this.yt = {
             height:100,
             width:100,
@@ -10,11 +12,9 @@
         };
 
         this.sendControlEvent = function(YT_event){
-            console.log(YT_event);
             $scope.$broadcast(YT_event);
         }
 
-        this.YT_events = YT_events;
     }
 
     angular.module('youtube.controllers.module').controller('YouTubeController',['$scope','YT_events', YouTubeController]);
